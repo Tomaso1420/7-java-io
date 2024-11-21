a7db10f9a3361e678b0e304d0da0b913853098cc
Tomaso1420 <Artem82005@mail.ru>
2024-11-21 16:42:20 +0500

    Марциохо задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,22 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        InputStream inputStream = System.in;
+
+        int first = inputStream.read();
+        int second = inputStream.read();
+
+        while (first != -1) {
+            if (first != 13 || second != 10) {
+                System.out.write(first);
+            }
+            first = second;
+            second = inputStream.read();
+        }
+        System.out.flush();
     }
 }
